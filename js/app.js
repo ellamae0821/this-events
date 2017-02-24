59cc5a7c57b4273792c1474c404ea9b10e7aa882
ella <ellamae207@gmail.com>
2017-02-23 21:36:59 -1000

    DONE

--- js/app.js
+++ js/app.js
@@ -2,3 +2,30 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+// var hElement = document.getElementsByClassName("store")
+
+// for (var i=0; i<hElement.length; i++){
+//   hElement[i].addEventListener("click", makePretty);
+// }
+
+// function makePretty(){
+//   var flavorElem = this.querySelectorAll(".flavor")[0];
+//   if(flavorElem.style.color === "black"){
+//     flavorElem.style.color = "green";
+//   }else{
+//     flavorElem.style.color = "black"
+//   }
+// }
+var restoName = document.getElementsByClassName("name");
+for (var i=0; i<restoName.length; i++){
+	restoName[i].addEventListener("click", unHide);
+}
+
+function unHide (){
+	var showMenu = this.querySelectorAll(".menu")[0];
+	if(showMenu.style.display === "none"){
+		showMenu.style.display = "block";
+	}else{
+		showMenu.style.display = "none";
+	}
+}
